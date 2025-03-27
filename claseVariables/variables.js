2dfb156372fadeaa49b1538318c86911641c21a0
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-03-27 13:37:01 -0300

    first commit

---
+++ claseVariables/variables.js
@@ -0,0 +1,22 @@
+let cajaDeAndy = "Woody"; //let es una variable que puede cambiar con el tiempo 
+const caja2DeAndy = "Buzz"; //const es una variable que no puede cambiar con el tiempo
+var caja3DeAndy = "Jessie"; //var es una variable que puede cambiar con el tiempo
+
+
+
+
+console.log(cajaDeAndy); // Woody
+
+
+// No permitido
+let c = 'Woody';
+let cda = 'Woody';
+let pcAndy = 'Woody';
+let c, cda
+    pcAndy = 'Woody';
+
+// Permitido
+let primerTrasteoDeAndy = 'Woody';
+let urlDelUsuario = 'https://www.google.com';
+let idDelUsuario = '1234567890';
+
