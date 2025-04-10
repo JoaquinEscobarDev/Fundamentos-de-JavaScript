dc0b2e79f86397f9807f6120ff50a1c04ea10617
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-04-10 01:15:47 -0400

    Add logical operators examples in JavaScript

---
+++ fundamentos/parte2/ciclos/operadores/logicos.js
@@ -0,0 +1,15 @@
+/*
+    Operadores Lógicos
+
+    &&
+    ||
+    !
+*/
+
+const a = 10;
+const b = 20;
+const c = '10';
+
+console.log(a == b && a === c); // false
+console.log(a == b || !(a === c)); // true
+console.log(a == b || a === c); // false
