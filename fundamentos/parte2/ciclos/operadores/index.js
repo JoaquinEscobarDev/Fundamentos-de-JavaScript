93d646296c15c45af6942900b47e9207d481a478
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-04-10 01:25:46 -0400

    Refactor comparison operator examples to clarify output comments

--- fundamentos/parte2/ciclos/operadores/index.js
+++ fundamentos/parte2/ciclos/operadores/index.js
@@ -65,20 +65,20 @@ console.log(null <= undefined); // false
 // Operador de comparación de objetos
 console.log({} == {}); // false
 console.log({} != {}); // true
-console.log({} === {}); // false
-console.log({} !== {}); // true
+// console.log({} === {}); // false
+// console.log({} !== {}); // true
 
 // Operador de comparación de arreglos
 console.log([] == []); // false
 console.log([] != []); // true
-console.log([] === []); // false
-console.log([] !== []); // true
+// console.log([] === []); // false
+// console.log([] !== []); // true
 
 // Operador de comparación de funciones
 console.log(function(){} == function(){}); // false
 console.log(function(){} != function(){}); // true
-console.log(function(){} === function(){}); // false
-console.log(function(){} !== function(){}); // true
+// console.log(function(){} === function(){}); // false
+// console.log(function(){} !== function(){}); // true
 
 // Operador de comparación de fechas
 console.log(new Date() == new Date()); // false
@@ -89,8 +89,8 @@ console.log(new Date() !== new Date()); // true
 // Operador de comparación de expresiones regulares
 console.log(/a/ == /a/); // false
 console.log(/a/ != /b/); // true
-console.log(/a/ === /a/); // false
-console.log(/a/ !== /b/); // true
+// console.log(/a/ === /a/); // false
+// console.log(/a/ !== /b/); // true
 
 // Operador de comparación de símbolos
 console.log(Symbol() == Symbol()); // false
