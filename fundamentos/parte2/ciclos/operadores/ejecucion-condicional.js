ab0e80386a1d10375cd5b469afd49ff6c35113df
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-04-10 01:25:57 -0400

    Add initial HTML structure and conditional execution example in JavaScript

---
+++ fundamentos/parte2/ciclos/operadores/ejecucion-condicional.js
@@ -0,0 +1,15 @@
+/*
+Condicional IF
+El condicional if se utiliza para ejecutar un bloque de código si una condición es verdadera.
+*/
+
+let nombre = "Diego";
+
+if (nombre === "Diego") {
+    console.log("Hola Diego");
+}else if (nombre === "Juan") {
+    console.log("Hola Juan");
+}else{
+    console.log("No eres Diego o Juan");
+
+}
