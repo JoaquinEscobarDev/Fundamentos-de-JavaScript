c1ef65c75084412d254c04c99392665f5c9771e3
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-04-14 23:02:18 -0400

    Add switch statement example for fruit pricing

---
+++ fundamentos/parte2/ciclos/ejecucion-condicional/switch.js
@@ -0,0 +1,32 @@
+// switch(expresion){
+//     case valor1:
+//         // Código a ejecutar si la expresión es igual a valor1
+//         break;
+//     case valor2:
+//         // Código a ejecutar si la expresión es igual a valor2
+//         break;
+//     case valor3:
+//         // Código a ejecutar si la expresión es igual a valor3
+//         break;
+//     default:
+//         // Código a ejecutar si la expresión no coincide con ninguno de los casos anteriores
+//         break;
+
+// }
+
+let expr = "Papaya";
+
+switch (expr) {
+    case "Banana":
+        console.log("¡La banana sale 205 pesos!");
+        break;
+    case "Manzana":
+        console.log("¡La manzana sale 205 pesos!");
+        break;
+    case "Papaya":
+        console.log("¡La papaya sale 205 pesos!");
+        break;
+    default:
+        console.log("No se encontro resultado.");
+        break;
+}
