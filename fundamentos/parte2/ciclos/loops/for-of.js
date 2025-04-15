7ffd121ac44ebfd4e02fa0548f6baae2b0539fe7
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-04-14 23:19:26 -0400

    Add example of for-of loop to iterate over an array of fruits

---
+++ fundamentos/parte2/ciclos/loops/for-of.js
@@ -0,0 +1,13 @@
+/*  
+for of arrays, strings [algo]
+
+for (variable of objeto) {
+    // código
+}
+*/
+
+let canasta = ['manzana', 'pera', 'uva', 'sandía'];
+
+for (fruta of canasta){ //por cada fruta de la canasta realiza esta acción
+    console.log(fruta);
+}
