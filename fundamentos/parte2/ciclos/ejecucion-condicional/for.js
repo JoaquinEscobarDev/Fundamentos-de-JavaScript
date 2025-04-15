9da16d65fec225e883eca832ca2024bc0c2820b8
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-04-14 23:12:34 -0400

    Add example of for loop to iterate over a list of fruits

---
+++ fundamentos/parte2/ciclos/ejecucion-condicional/for.js
@@ -0,0 +1,17 @@
+// for(variable; condicion; incremento o decremento){
+//     // Bloque de código a ejecutar
+// }
+// // Ejemplo de un bucle for que imprime los números del 1 al 5
+
+let list = [
+    "Manzana",
+    "Pera",
+    "Banana",
+    "Papaya",
+    "Sandía"
+]
+for (let i=0; i < list.length; i++){
+
+    console.log(list[i]);
+
+}
