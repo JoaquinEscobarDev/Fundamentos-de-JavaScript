52186197149a326a2a5035a527fc8a2ce5397f7d
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-04-14 23:53:05 -0400

    Add example of do-while loop to demonstrate execution at least once

---
+++ fundamentos/parte2/ciclos/loops/doWhile.js
@@ -0,0 +1,18 @@
+/*
+
+do {
+    // Bloque de código a ejecutar
+} while(condicion);
+// El bloque de código se ejecuta al menos una vez, incluso si la condición es falsa.
+// La condición se evalúa después de ejecutar el bloque de código.
+
+
+*/
+
+let contador = 0;
+
+do{
+    console.log(contador);
+    contador++;
+
+}while(contador < 10);
